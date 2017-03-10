cc7031f3cb33f8e6c2a41d5db23b7015dc094924
Chris Aquino <caquino@bignerdranch.com>
2017-03-10 14:58:20 -0500

    first steps with react and react-storybook

---
+++ src/components/Row.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Row = () => (
+  <div>
+    <input type="checkbox" />
+    Fancy coffee number one x43
+  </div>
+);
+
+export default Row;
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Button from './Button';
-import Welcome from './Welcome';
 
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
+import Row from '../components/Row';
+
+// import Button from './Button';
+// import Welcome from './Welcome';
+
+storiesOf('Row', module)
+  .add('with stuff', () => (
+    <Row />
   ));
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  ));
+// storiesOf('Welcome', module)
+//   .add('to Storybook', () => (
+//     <Welcome showApp={linkTo('Button')}/>
+//   ));
+
+// storiesOf('Button', module)
+//   .add('with text', () => (
+//     <Button onClick={action('clicked')}>Hello Button</Button>
+//   ))
+//   .add('with some emoji', () => (
+//     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+//   ));
